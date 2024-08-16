2e399bbee12c70f5e68197f3b51469db4d80025d
Ema-42 <emanuelchusgp@gmail.com>
2024-08-16 18:15:15 -0400

    agregando jwt , cookies en login y pagina protegida

--- config.js
+++
@@ -1,1 +0,0 @@
-export const { PORT = 3000, SALT_ROUNDS = 10 } = process.env;
--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, SECRET_JWT_KEY } from "./config.js";
 import { UserRepository } from "./user-repository.js";
+import cookieParser from "cookie-parser";
+
+import jwt from "jsonwebtoken";
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.static('public'));
+app.use(cookieParser());
+app.use(express.static("public"));
 
-
-app.get("/", (request, response) => {
-  response.render("index", { username: "emanuel mario" });
+app.get("/", (req, res) => {
+  const token = req.cookies.access_token;
+  if (!token) return res.render("index");
+  try {
+    const data = jwt.verify(token, SECRET_JWT_KEY);
+    res.render("index", data);
+  } catch (error) {
+    res.render("index");
+  }
 });
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   try {
     const user = await UserRepository.login({ username, password });
-    res.send({ user });
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      SECRET_JWT_KEY,
+      { expiresIn: "1h" }
+    );
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 1000 * 60 * 60,
+      })
+      .send({ user, token });
   } catch (error) {
-    res.status(401).send(error.message);
+    res.status(401).json({ error: error.message });
   }
 });
 
@@ -34,8 +56,19 @@ app.post("/register", async (req, res) => {
   }
 });
 app.post("/logout", (req, res) => {});
-app.get("/protected", (req, res) => {});
 
+app.get("/protected", (req, res) => {
+  const token = req.cookies.access_token;
+  if (!token) {
+    return res.status(403).send("Acceso no autorizado");
+  }
+  try {
+    const data = jwt.verify(token, SECRET_JWT_KEY);
+    res.render("protected", data);
+  } catch (error) {
+    res.status(401).send("Acceso no autorizado");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`corriendo en el puerto ${PORT}`);
